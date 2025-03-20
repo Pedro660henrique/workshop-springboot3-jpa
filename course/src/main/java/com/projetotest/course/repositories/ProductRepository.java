53eb85a702fcfde533d59b41e13fedfa05a08984
Pedro660henrique <pedro.santos660@hotmail.com>
2025-03-20 12:22:34 -0300

    Product

---
+++ course/src/main/java/com/projetotest/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.projetotest.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetotest.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+		
+}
